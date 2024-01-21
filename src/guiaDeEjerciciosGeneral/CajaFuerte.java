8a76b117d42b0ccf4aeb01992ca9ba36f19b17c8
Veronica <dv.veronica2075@gmail.com>
2024-01-21 18:21:33 -0300

    Salario Semanal

---
+++ src/guiaDeEjerciciosGeneral/CajaFuerte.java
@@ -0,0 +1,9 @@
+/*
+5 . Realiza el control de acceso a una caja fuerte donde la combinación será ingresada por teclado, un número de 4
+cifras. Si no acertamos, se nos mostrará el mensaje “Lo siento, esa no es la combinación” y si acertamos se nos dirá “La caja fuerte se ha abierto satisfactoriamente”. Tendremos cuatro oportunidades para abrir la caja fuerte.
+*/
+
+package guiaDeEjerciciosGeneral;
+
+public class CajaFuerte {
+}
